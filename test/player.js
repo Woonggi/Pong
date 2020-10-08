9ab4f96d0593c30a6ca09828492dcd63be3b8a0b
unknown <doubleugeun@gmail.com>
2020-10-08 15:12:31 -0700

    paddle moving test

--- test/player.js
+++ test/player.js
@@ -1,11 +1,8 @@
 module.exports = class player {
-    constructor(xpos = 0, ypos = 0) {
+    constructor() {
         this.to_trans = {};
-        this.to_trans.x = xpos;
-        this.to_trans.y = ypos;
-        this.width = 20;
-        this.height = 100;
-        this.randnum = Math.random(),
+        this.to_trans.x = 20;
+        this.to_trans.y = 330;
         this.keypress = [];
     }
 };
--- test/server.js
+++ test/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const player_instance = require('./player.js');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -7,35 +6,36 @@ const player = require('./player.js');
 
 players = {};
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
     players[socket.id]  = new player();
+    console.log('user connected', socket.id);
     io.emit('create', socket.id);
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', () => {
         delete players[socket.id];
         io.emit('disconnect', socket.id);
         console.log('user disconnected', socket.id);
     });
 
-    socket.on('keydown', function(keycode) {
+    socket.on('keydown', (keycode) => {
         players[socket.id].keypress[keycode] = true;
-    })
+    });
 
-    socket.on('keyup', function(keycode) {
-        players[socket.id].keypress[keycode] = false;
-    })
+    socket.on('keyup', (keycode) => {
+            players[socket.id].keypress[keycode] = false;
+    });
 
 });
 
 const UP = 38, DOWN = 40;
-var update = setInterval(function(){
+var update = setInterval(() => {
     // check if players are connected or disconnceted all the time.
-    var status = {};
-    var ids = [];
+    let status = {};
+    let ids = [];
     for(var id in io.sockets.clients().connected) {
         if(players[id].keypress[UP]) {
             players[id].to_trans.y -= 7;
@@ -50,6 +50,6 @@ var update = setInterval(function(){
 }, 10);
 
 var port = 3000;
-http.listen(port, function() {
+http.listen(port, () => {
     console.log("listening on port " + port);
 });
