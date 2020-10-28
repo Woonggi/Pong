38feee3a1c23d4ed06ff46ea55a136345b08fef8
unknown <doubleugeun@gmail.com>
2020-10-27 19:57:23 -0700

    Fixing bugs that game doesn't work properly when a player scores

--- test/ball.js
+++ test/ball.js
@@ -10,8 +10,6 @@ module.exports = class ball{
         // For testing, it is hard-coded now.
         this.side = 20;
     }
-
-
     update(left_player, right_player, curr_state) {
         let pi = Math.PI;
 
@@ -19,44 +17,57 @@ module.exports = class ball{
         const WIDTH = 700;
         const HEIGHT = 600;
 
-        // score condition
-        if(this.to_trans.x  <= 0) {
-            curr_state = "ST_IDLE"
+        if(curr_state === "ST_IDLE") {
+            this.vel_x = 0;    
+            this.vel_y = 0;    
+        }
+        else if(curr_state === "ST_LEFTBALL") {
             this.to_trans.x = left_player.width + 5;
-            this.to_trans.y = left_player.to_trans.y;
-            right_player.to_trans.points++;
-            console.log(left_player.to_trans.points + " : " + right_player.to_trans.points);
+            this.to_trans.y = left_player.to_trans.y + left_player.height / 2;
+            this.vel_x = 0;    
+            this.vel_y = 0;    
         }
-        if (this.to_trans.x > WIDTH) {
-            curr_state = "ST_IDLE"
-            this.to_trans.x = right_player.width - 5;
-            this.to_trans.y = right_player.to_trans.y;
-            left_player.to_trans.points++;
-            console.log(left_player.to_trans.points + " : " + right_player.to_trans.points);
+        else if (curr_state === "ST_RIGHTBALL") {
+            this.to_trans.x = WIDTH - right_player.width - 5;
+            this.to_trans.y = right_player.to_trans.y + right_player.height / 2;
+            console.log(this.to_trans.x, this.to_trans.y);
+            this.vel_x = 0;    
+            this.vel_y = 0;    
         }
+        else if (curr_state === "ST_ONGAME") {
+            this.to_trans.x += this.vel_x;
+            this.to_trans.y += this.vel_y;
+            // score condition
+            if (this.to_trans.x <= 0) {
+                curr_state = "ST_LEFTBALL"
+                right_player.to_trans.points += 1;
+                console.log(left_player.to_trans.points + " : " + right_player.to_trans.points);
+            }
+            if (this.to_trans.x > WIDTH) {
+                curr_state = "ST_RIGHTBALL"
+                left_player.to_trans.points += 1;
+                console.log(left_player.to_trans.points + " : " + right_player.to_trans.points);
+            }
+            // proceed physics only if game is on going
+            if (this.to_trans.y < 0 || this.to_trans.y + this.side > HEIGHT) {
+                var offset = this.vel_y < 0 ? 0 - this.to_trans.y : HEIGHT - (this.to_trans.y + this.side);
+                this.to_trans.y += 2 * offset;
+                this.vel_y *= -1;
+            }
 
-        this.to_trans.x += this.vel_x;
-        this.to_trans.y += this.vel_y;
+            var AABBIntersection = (ax, ay, aw, ah, bx, by, bw, bh) => {
+                return ax < bx + bw && ay < by + bh && bx < ax + aw && by < ay + ah;
+            }
 
-        // proceed physics only if game is on going
-        if(this.to_trans.y < 0 || this.to_trans.y + this.side > HEIGHT) {
-            var offset = this.vel_y < 0 ? 0 - this.to_trans.y : HEIGHT - (this.to_trans.y + this.side);
-            this.to_trans.y += 2 * offset;
-            this.vel_y *= -1;
-            console.log(this.vel_y);
+            var paddle = this.vel_x < 0 ? left_player : right_player;
+            if (AABBIntersection(paddle.to_trans.x, paddle.to_trans.y, paddle.width, paddle.height, this.to_trans.x, this.to_trans.y, this.side, this.side)) {
+                this.to_trans.x = paddle === left_player ? left_player.to_trans.x + left_player.width : right_player.to_trans.x - this.side;
+                var n = (this.to_trans.y + this.side - paddle.to_trans.y) / (paddle.height + this.side);
+                var phi = 0.25 * pi * (2 * n - 1);
+                this.vel_x = (paddle === left_player ? 1 : -1) * this.speed * Math.cos(phi);
+                this.vel_y = this.speed * Math.sin(phi);
+            }
         }
-
-        var AABBIntersection = (ax, ay, aw, ah, bx, by, bw, bh) => {
-            return ax < bx + bw && ay < by + bh && bx < ax + aw && by < ay + ah;
-        }
-
-        var paddle = this.vel_x < 0 ? left_player : right_player;
-        if (AABBIntersection(paddle.to_trans.x, paddle.to_trans.y, paddle.width, paddle.height, this.to_trans.x, this.to_trans.y, this.side, this.side)) {
-            this.to_trans.x = paddle === left_player ? left_player.to_trans.x + left_player.width : right_player.to_trans.x - this.side;
-            var n = (this.to_trans.y + this.side - paddle.to_trans.y) / (paddle.height + this.side);
-            var phi = 0.25 * pi * (2 * n - 1);
-            this.vel_x = (paddle === left_player ? 1 : -1) * this.speed * Math.cos(phi);
-            this.vel_y = this.speed * Math.sin(phi);
-        }
+        //console.log(curr_state);
     }
 };
--- test/server.js
+++ test/server.js
@@ -5,24 +5,21 @@ const io = require('socket.io')(http);
 const player = require('./player.js');
 const ball_obj = require('./ball.js')
 
-let players = {};
-let player1_id, player2_id;
-let ball = {};
-let num_player = 1;
-
-// TODO: server send clients respect to setup parameters
-
 const setups = {
     width : 700,
     height: 600
 }
 
-const game_state = {
-    ST_IDLE : "ST_IDLE",
-    ST_ONGAME : "ST_ONGAME" 
-}
+const game_state = ["ST_IDLE", "ST_ONGAME", "ST_LEFTBALL", "ST_RIGHTBALL"]
 
-let curr_state = game_state.ST_IDLE; 
+let curr_state = game_state[0]; 
+let players = {};
+let ball = new ball_obj((setups.width - 20) / 2, (setups.height - 20) / 2);
+let player1_id, player2_id;
+let num_player = 1;
+
+// TODO: server send clients respect to setup parameters
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -31,17 +28,16 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     io.emit('setup', setups)
     if(num_player == 1) {
-        players[socket.id]  = new player(20, 280, num_player);
+        players[socket.id]  = new player(20, (setups.height - 100)/ 2, num_player);
         player1_id = socket.id;
         console.log(player1_id);
-        ball = new ball_obj(setups.width/ 2, setups.height/ 2);
         console.log(num_player + "P")
         num_player++;
     }
     else if(num_player == 2) {
         player2_id = socket.id;
         console.log(player2_id);
-        players[socket.id] = new player(660, 280, num_player);
+        players[socket.id] = new player(setups.width - 40, (setups.height - 100) / 2, num_player);
     }
 
     console.log('user connected', socket.id, num_player);
@@ -77,16 +73,16 @@ var update = setInterval(() => {
             players[id].to_trans.y+= 7;
         }
         if(players[id].keypress[SPACE] && 
-            curr_state == game_state.ST_IDLE && num_player == 2) {
-            curr_state = game_state.ST_ONGAME;
+            curr_state === "ST_IDLE" && num_player == 2) {
+            ball.vel_x = ball.speed;
+            curr_state = "ST_ONGAME";
         }
 
         ids.push(id);
         status[id] = players[id].to_trans;
     }
-    if (curr_state == game_state.ST_ONGAME) {
-        ball.update(players[player1_id], players[player2_id], curr_state);
-    }
+    ball.update(players[player1_id], players[player2_id], curr_state);
+   
     io.emit('update', ids, status, ball.to_trans);
 }, 30);
 
